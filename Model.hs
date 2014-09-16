be47a2c362325c7af2adbd9042ccbcdba009e3e2
karsten gebbert <k@ioctl.it>
2014-09-16 17:17:33 +0200

    template import

---
+++ Model.hs
@@ -0,0 +1,17 @@
+module Model where
+
+import Yesod
+import Data.Text (Text)
+import Database.Persist.Quasi
+import Data.Typeable (Typeable)
+import Data.Int
+import Text.Show
+import Data.Bool
+       
+
+-- You can define all of your database entities in the entities file.
+-- You can find more information on persistent and how to declare entities
+-- at:
+-- http://www.yesodweb.com/book/persistent/
+share [mkPersist sqlSettings, mkMigrate "migrateAll"]
+    $(persistFileWith lowerCaseSettings "config/models")
